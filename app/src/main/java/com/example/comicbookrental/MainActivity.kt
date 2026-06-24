51dc3f730e801b98badc5d6f5220faa99bf7e50c
Nguyen Duc Anh <nguyenanhduc2938@gmail.com>
2026-06-24 16:17:26 +0700

    Nguyen Duc Anh 206 - fix: resolve conflict

--- app/src/main/java/com/example/comicbookrental/MainActivity.kt
+++ app/src/main/java/com/example/comicbookrental/MainActivity.kt
@@ -4,18 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.comicbookrental.data.mock.AuthMockData
-import com.example.comicbookrental.ui.screens.reader_screen.ReaderScreen
 import com.example.comicbookrental.ui.theme.ComicBookRentalTheme
-import com.example.comicbookrental.ui.screens.reset_password.ResetPasswordScreen
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.rememberNavController
-import com.example.comicbookrental.ui.navigation.CatalogGraph
-import com.example.comicbookrental.ui.navigation.catalogGraph
-import com.example.comicbookrental.ui.screens.search.SearchRoute
-import com.example.comicbookrental.data.mock.AuthMockData
 import com.example.comicbookrental.ui.navigation.AppNavHost
-import com.example.comicbookrental.ui.theme.ComicBookRentalTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
